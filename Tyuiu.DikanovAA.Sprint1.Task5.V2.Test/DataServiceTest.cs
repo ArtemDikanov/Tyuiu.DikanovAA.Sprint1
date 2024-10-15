c368e97b2a3c09841836460124e98312f3f44fe0
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-10-15 12:13:54 +0500

    Task5

---
+++ Tyuiu.DikanovAA.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.DikanovAA.Sprint1.Task5.V2.Lib;
+namespace Tyuiu.DikanovAA.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double temp = 68;
+            DataService ds = new DataService();
+            double res = ds.FahrenheitTo—elsius(temp);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 20;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.DikanovAA.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.DikanovAA.Sprint1.Task5.V2.Lib;
+namespace Tyuiu.DikanovAA.Sprint1.Task5.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Диканов А. А. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразования типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Диканов Артём Алексеевич | РППб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано значение температуры в градусах Фаренгейта.                        *");
+            Console.WriteLine("* Определить значение этой же температуры в градусах Цельсия.             *");
+            Console.WriteLine("* Ответ привести к целому с помощью класса Convert.                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение температуры в градусах Фаренгейта: ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = Convert.ToInt32(ds.FahrenheitToСelsius(temp));
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
