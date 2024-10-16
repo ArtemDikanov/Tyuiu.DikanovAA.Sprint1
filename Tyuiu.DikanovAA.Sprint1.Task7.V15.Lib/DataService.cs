1317ee2c4a2366703acfda25faae5da61d620a67
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-10-16 12:30:58 +0500

    Task7

--- Tyuiu.DikanovAA.Sprint1.Task7.V15.Lib/DataService.cs
+++ Tyuiu.DikanovAA.Sprint1.Task7.V15.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.DikanovAA.Sprint1.Task7.V15.Lib
         public double Calculate(double x)
         {
             double res = (Math.Abs(Math.Pow(x, 2) - Math.Pow(x, 3))) - ((Math.Cos(Math.Pow(x, 3)) + 7 * Math.Pow(x, 2)) / (Math.Pow(x, 3) - 15 * x));
-            return res;
+            return Math.Round(res, 3);
         }
     }
 }
